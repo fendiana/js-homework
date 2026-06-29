5b553dbfa6b50856aee945c27b1fa3273417d7ed
fendiana <diana.fen02@gmail.com>
2026-06-29 23:32:10 +0200

    Update script.js

--- Lesson2/HomeWork/js/script.js
+++ Lesson2/HomeWork/js/script.js
@@ -1,15 +1,154 @@
 // Якщо змінна більше нуля - виведіть true, менше - false
 //Перевірте це на варіантах  1, 0, -3.
 
+// let value = 1;
+// if (value > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// let value = 0;
+// if (value > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// let value = -3;
+// if (value > 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
 // Якщо змінна ="test" - виведіть true,
 //Перевірте це на варіантах  'test', "qwerty", true
 
+// let text = "test";
+// if (text === "test") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// let text = "qwerty";
+// if (text === "test") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// let text = "true";
+// if (text === "test") {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
 // Якщо змінна більше 10 -  відніміть 5,
-//менше - додайте 5, результат виведіть в консоль
+// менше - додайте 5, результат виведіть в консоль
 //Перевірте це на варіантах  1, 10, 13.
 
-//Зробіть сервіс який отримує число від 1 до 12
-// виведіть місяць який дорівнює числу
+// let value = 1;
+// if (value > 10) {
+//   console.log(value - 5);
+// } else if (value < 10) {
+//   console.log(value + 5);
+// } else {
+//   console.log(value);
+// }
 
-//Зробіть сервіс який отримує тризначне число
+// let value = 10;
+// if (value > 10) {
+//   console.log(value - 5);
+// } else if (value < 10) {
+//   console.log(value + 5);
+// } else {
+//   console.log(value);
+// }
+
+// let value = 13;
+// if (value > 10) {
+//   console.log(value - 5);
+// } else if (value < 10) {
+//   console.log(value + 5);
+// } else {
+//   console.log(value);
+// }
+
+// Зробіть сервіс який отримує число від 1 до 12
+//Виведіть місяць який дорівнює числу
+
+// const month = Number(prompt("Введіть місяць народження числом"));
+
+// const month = prompt("Введіть місяць народження числом");
+// let name;
+
+// switch (month) {
+//   case "1":
+//     name = "січень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "2":
+//     name = "лютий";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "3":
+//     name = "березень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "4":
+//     name = "квітень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "5":
+//     name = "травень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "6":
+//     name = "червень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "7":
+//     name = "липень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "8":
+//     name = "серпень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "9":
+//     name = "вересень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "10":
+//     name = "жовтень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "11":
+//     name = "листопад";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   case "12":
+//     name = "грудень";
+//     alert(`Місяць під номером ${month} - це ${name}`);
+//     break;
+
+//   default:
+//     alert(`Не існує мйсяця під номером ${month}`);
+// }
+
+// Зробіть сервіс який отримує тризначне число
 //Поверніть користувачу сумму цих чисел
