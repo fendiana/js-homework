90b5a3273ca13c495a785217656c747f110c22af
fendiana <diana.fen02@gmail.com>
2026-07-30 13:54:28 +0200

    Add lesson26

---
+++ Lesson26/HomeWork/js/script.js
@@ -0,0 +1,40 @@
+// --1---
+// Створити телефонну книгу:
+// - створити початковий клас Abonent, де зберігатимуться ім*я і номер
+// - створити set який прийматиме номер телефону
+// - створити get який виводитиме дані про абонента
+// - створити три різних юзери вивести дані
+
+class Abonent {
+  constructor(options) {
+    this.name = options.name;
+    this.number = options.number;
+  }
+
+  set phoneNum(value) {
+    this.number = value;
+  }
+
+  get abonentInfo() {
+    return `Name: ${this.name} Number: ${this.number}`;
+  }
+}
+
+const user1 = new Abonent({
+  name: "Sam",
+  number: "+123456789012",
+});
+
+const user2 = new Abonent({
+  name: "Paul",
+  number: "+234567890123",
+});
+
+const user3 = new Abonent({
+  name: "Jack",
+  number: "+345678901234",
+});
+
+console.log(user1.abonentInfo);
+console.log(user2.abonentInfo);
+console.log(user3.abonentInfo);
