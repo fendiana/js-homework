e00ee1e4a2db0a97ca3861693e5458674de956b3
fendiana <diana.fen02@gmail.com>
2026-08-02 07:24:58 +0200

    Add lesson 27

--- Lesson26/HomeWork/js/script.js
+++ Lesson26/HomeWork/js/script.js
@@ -5,36 +5,36 @@
 // - створити get який виводитиме дані про абонента
 // - створити три різних юзери вивести дані
 
-class Abonent {
-  constructor(options) {
-    this.name = options.name;
-    this.number = options.number;
-  }
+// class Abonent {
+//   constructor(options) {
+//     this.name = options.name;
+//     this.number = options.number;
+//   }
 
-  set phoneNum(value) {
-    this.number = value;
-  }
+//   set phoneNum(value) {
+//     this.number = value;
+//   }
 
-  get abonentInfo() {
-    return `Name: ${this.name} Number: ${this.number}`;
-  }
-}
+//   get abonentInfo() {
+//     return `Name: ${this.name} Number: ${this.number}`;
+//   }
+// }
 
-const user1 = new Abonent({
-  name: "Sam",
-  number: "+123456789012",
-});
+// const user1 = new Abonent({
+//   name: "Sam",
+//   number: "+123456789012",
+// });
 
-const user2 = new Abonent({
-  name: "Paul",
-  number: "+234567890123",
-});
+// const user2 = new Abonent({
+//   name: "Paul",
+//   number: "+234567890123",
+// });
 
-const user3 = new Abonent({
-  name: "Jack",
-  number: "+345678901234",
-});
+// const user3 = new Abonent({
+//   name: "Jack",
+//   number: "+345678901234",
+// });
 
-console.log(user1.abonentInfo);
-console.log(user2.abonentInfo);
-console.log(user3.abonentInfo);
+// console.log(user1.abonentInfo);
+// console.log(user2.abonentInfo);
+// console.log(user3.abonentInfo);
---
+++ Lesson27/HomeWork/js/script.js
@@ -0,0 +1,27 @@
+// ----1----
+// Створіть функцію - конструктор Calculator, який створює об’єкти з трьома методами:
+// read() запитує два значення за допомогою prompt і запам’ятовує їх у властивостях об’єкта.
+// sum() повертає суму цих властивостей.
+// mul() повертає результат множення даних властивостей.
+
+// function Calculator() {
+//   this.read = function () {
+//     this.a = +prompt("Введіть перше значення");
+//     this.b = +prompt("Введіть друге значення");
+//   };
+
+//   this.sum = function () {
+//     return this.a + this.b;
+//   };
+
+//   this.mul = function () {
+//     return this.a * this.b;
+//   };
+// }
+
+// let calculator = new Calculator();
+
+// calculator.read();
+
+// console.log("Сума чисел: " + calculator.sum());
+// console.log("Добуток чисел: " + calculator.mul());
